ed33dc2c66233b171d8ad47cbd58a09cac25c16f
Sandra <sandyjtech@gmail.com>
2023-04-13 17:06:44 -0700

    added array functions

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+ function  destructivelyPrependCat() {
+    return cats.unshift("Bob");
+ }
+ function destructivelyAppendCat() {
+    return cats.push("Ralph");
+}
+ function  destructivelyRemoveLastCat() {
+    return cats.pop();
+ }
+
+ function destructivelyRemoveFirstCat(){
+return cats.shift();
+ }
+function appendCat(name) {
+ return   [...cats, "Broom" ]} 
+
+function prependCat(name){
+     return ["Arnold", ...cats];
+   }
+   
+   function  removeLastCat() {
+return cats.slice(0, cats.length -1) ;
+   }
+   function removeFirstCat() {
+return cats.slice(1);
+   }
